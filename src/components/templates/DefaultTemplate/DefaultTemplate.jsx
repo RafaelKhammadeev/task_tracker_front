16ba15b56bcf95c4a1e53e665951b5c687b35d5e
Rafael <msmk.swimmer@gmail.com>
2023-03-27 21:57:59 +0300

    do hw06

--- src/components/templates/DefaultTemplate/DefaultTemplate.jsx
+++ src/components/templates/DefaultTemplate/DefaultTemplate.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Header from "../../molecules/Header";
 import Footer from "../../molecules/Footer";
+import Button from "../../atoms/Button";
 
 const MainContent = styled.div`
   background: white;
 `;
 
+const ButtonWrapper = styled.div`
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+`;
+
 const DefaultTemplate = ({ children }) => {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  const onScroll = () => {
+    if (window.scrollY > 0) {
+      setShowScrollButton(true);
+    } else {
+      setShowScrollButton(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", onScroll);
+  }, []);
+
+  const onClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div>
       <Header />
+      {showScrollButton && (
+        <ButtonWrapper>
+          <Button onClick={onClick} label="Scroll to top" color="black" />
+        </ButtonWrapper>
+      )}
       <MainContent>{children}</MainContent>
       <Footer />
     </div>
