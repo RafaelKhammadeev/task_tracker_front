17c4bcbe2baf034236a26674e61f268bf5e6092c
Rafael <msmk.swimmer@gmail.com>
2023-04-17 15:21:51 +0300

    delete mock-data

--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -11,68 +11,10 @@ const GroupCard = styled.div`
   justify-content: center;
 `;
 
-const mockProjects = [
-  {
-    id: 1,
-    name: "Project #1",
-    description: "It's my first",
-    createdAt: "22 days ago",
-  },
-  {
-    id: 2,
-    name: "Project #1",
-    description: "It's my first project",
-    createdAt: "22 days ago",
-  },
-  {
-    id: 3,
-    name: "Project #1",
-    description: "It's my first",
-    createdAt: "22 days ago",
-  },
-  {
-    id: 4,
-    name: "Project #1",
-    description: "It's my first project",
-    createdAt: "22 days ago",
-  },
-  {
-    id: 5,
-    name: "Project #1",
-    description: "It's my first",
-    createdAt: "22 days ago",
-  },
-  {
-    id: 6,
-    name: "Project #1",
-    description: "It's my first project",
-    createdAt: "22 days ago",
-  },
-  {
-    id: 7,
-    name: "Project #1",
-    description: "It's my first",
-    createdAt: "22 days ago",
-  },
-  {
-    id: 8,
-    name: "Project #1",
-    description: "It's my first project",
-    createdAt: "22 days ago",
-  },
-  {
-    id: 9,
-    name: "Project #1",
-    description: "It's my first project",
-    createdAt: "22 days ago",
-  },
-];
-
 const ProjectsPage = () => {
-  const projects = useProjects();
-  console.log(projects);
+  const { projects } = useProjects();
 
-  const groupProjectCard = mockProjects.map((project) => (
+  const groupProjectCard = projects.map((project) => (
     <Card key={project.id} name={project.name} description={project.description} createdAt={project.createdAt} />
   ));
   return (
